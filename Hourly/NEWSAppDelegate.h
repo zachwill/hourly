7403207ba64fd1039eec6860eaf9059eaec73514
Zach Williams <hey@zachwill.com>
2012-11-15 15:01:37 -0800

    Initial Commit

---
+++ Hourly/NEWSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NEWSAppDelegate.h
+//  Hourly
+//
+//  Created by Zach Williams on 11/15/12.
+//  Copyright (c) 2012 Zach Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NEWSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hourly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hourly
+//
+//  Created by Zach Williams on 11/15/12.
+//  Copyright (c) 2012 Zach Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NEWSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NEWSAppDelegate class]));
+    }
+}
